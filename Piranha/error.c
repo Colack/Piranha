43340ba16be6dd0d30471b08db4dd08e408e0b61
Jack Spencer <UniformKitten281@gmail.com>
2022-10-03 20:48:11 -0700

    Create error.c

---
+++ Piranha/error.c
@@ -0,0 +1,14 @@
+#include "error.h";
+
+void error(int line, char error[MAX_STRING]) {
+    printf("%s Error : \n",PROGRAM_NAME);
+    printf("Line %d : %s\n",line,error);
+}
+
+void bypassError(int line, char error[MAX_STRING], char reason[MAX_STRING], bool show) {
+    printf("Skipped Error : Line : %d\n",error);
+    if (show) {
+        printf("%s Error : \n",PROGRAM_NAME);
+        printf("Line %d : %s\n",line,error);
+    }
+}
