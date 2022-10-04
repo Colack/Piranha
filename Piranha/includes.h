ab3f3883c04d09f440b1bdb5f346ddf9035e9afe
Jack Spencer <UniformKitten281@gmail.com>
2022-10-03 20:48:01 -0700

    Update includes.h

--- Piranha/includes.h
+++ Piranha/includes.h
@@ -5,7 +5,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+// Piranha Information
 #define MAX_STRING 100
+#define FILE_TYPE ".pr"
+
+// Maintenence Information
 #define CURRENT_VERSION "1.0.0"
 #define PROGRAM_NAME "Piranha"
 #define DOCUMENTATION "link goes here"
