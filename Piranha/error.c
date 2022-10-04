b716fb1976458ae2afc8746c24bd9210ea7f8739
Jack Spencer <UniformKitten281@gmail.com>
2022-10-04 12:59:11 -0700

    Update error.c

--- Piranha/error.c
+++ Piranha/error.c
@@ -6,7 +6,7 @@ void error(int line, char error[MAX_STRING]) {
 }
 
 void bypassError(int line, char error[MAX_STRING], char reason[MAX_STRING], bool show) {
-    printf("Skipped Error : Line : %d\n",error);
+    printf("Skipped Error : Line : %s\n",error);
     if (show) {
         printf("%s Error : \n",PROGRAM_NAME);
         printf("Line %d : %s\n",line,error);
