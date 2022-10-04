20e907e9339f3243db1b8c38a3017284c3e8cb57
Jack Spencer <UniformKitten281@gmail.com>
2022-10-04 09:49:25 -0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,14 @@
-cd ./Piranha
-gcc Piranha.c commands.c error.c -o Piranha
+@ECHO OFF
+  : Compile 
+
+  powershell write-host -fore green Compiling...
+  
+  cd ./Piranha
+  gcc Piranha.c commands.c error.c -o Piranha
+  
+  powershell write-host -fore green Compiled
+  
+  echo Press any key to Exit...
+  time 9999 > NUL
+  
+  EXIT /B 0
