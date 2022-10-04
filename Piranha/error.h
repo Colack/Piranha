528099e32b5b166c5080ce81e94d20f332bdc937
Jack Spencer <UniformKitten281@gmail.com>
2022-10-04 12:59:32 -0700

    Update error.h

--- Piranha/error.h
+++ Piranha/error.h
@@ -1,4 +1,4 @@
 #include "includes.h"
 
 void error(int line, char error[MAX_STRING]);
-void error(int line, char error[MAX_STRING], char reason[MAX_STRING], bool show);
+void bypassError(int line, char error[MAX_STRING], char reason[MAX_STRING], bool show);
