b6740eb62a53da53799e61ab0c813c9793033cb2
Jack Spencer <UniformKitten281@gmail.com>
2022-10-03 20:20:09 -0700

    Update includes.h

--- Piranha/includes.h
+++ Piranha/includes.h
@@ -3,5 +3,12 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+
+#define MAX_STRING 100
+#define CURRENT_VERSION "1.0.0"
+#define PROGRAM_NAME "Piranha"
+#define DOCUMENTATION "link goes here"
+#define MAINTAINER "Colack"
+#define YEAR 2022
+#define LICENSE "MIT"
