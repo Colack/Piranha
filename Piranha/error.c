f7927bd087fe9084d817afae8074120be7361013
Jack Spencer <UniformKitten281@gmail.com>
2022-10-05 13:20:39 -0700

    Update error.c

--- Piranha/error.c
+++ Piranha/error.c
@@ -1,14 +1,26 @@
 #include "error.h"
 
-void error(int line, char error[MAX_STRING]) {
-    printf("%s Error : \n",PROGRAM_NAME);
-    printf("Line %d : %s\n",line,error);
-}
+bool errorShown = true;
 
-void bypassError(int line, char error[MAX_STRING], char reason[MAX_STRING], bool show) {
-    printf("Skipped Error : Line : %s\n",error);
-    if (show) {
+void error(int line, char error[MAX_STRING]) {
+    if (errorShown) {
         printf("%s Error : \n",PROGRAM_NAME);
         printf("Line %d : %s\n",line,error);
     }
+}
+
+void bypassError(int line, char error[MAX_STRING], char reason[MAX_STRING]) {
+    printf("Skipped Error : Line : %s\n",error);
+    if (errorShown) {
+        printf("%s Error : \n",PROGRAM_NAME);
+        printf("Line %d : %s\n",line,error);
+    }
+}
+
+void hideErrors() {
+    errorShown = false;
+}
+
+void showErrors() {
+    errorShown = true;
 }
