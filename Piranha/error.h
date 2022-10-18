82ce79e0fe8a4a7d2acb8655eba960fce73c3101
Jack Spencer <UniformKitten281@gmail.com>
2022-10-18 09:41:40 -0700

    Delete error.h

--- Piranha/error.h
+++
@@ -1,8 +0,0 @@
-#include "includes.h"
-
-bool errorShown;
-
-void error(int line, char error[MAX_STRING]);
-void bypassError(int line, char error[MAX_STRING], char reason[MAX_STRING]);
-void hideErrors();
-void showErrors();
