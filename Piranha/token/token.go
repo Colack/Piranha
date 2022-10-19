0d31187cda913c3f14bf2188a98b7a3d3320ec1c
Jack Spencer <UniformKitten281@gmail.com>
2022-10-19 12:39:50 -0700

    Create token.go

---
+++ Piranha/token/token.go
@@ -0,0 +1,64 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	IDENT = "IDENT"
+	INT = "INT"
+	STRING = "STR"
+	BOOL = "BOOL"
+	ANY = "ANY"
+	CONST = "CONST"
+	NULL = "NULL"
+
+	ASSIGN = "="
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+
+	LT = "<"
+	GT = ">"
+
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fun": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+func LookUpIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
