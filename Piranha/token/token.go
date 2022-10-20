d1a6c028cb4062aced00b1e19a0df29d876d4507
Jack Spencer <UniformKitten281@gmail.com>
2022-10-20 16:48:06 -0700

    Update token.go

--- Piranha/token/token.go
+++ Piranha/token/token.go
@@ -1,118 +1,97 @@
 package token
 
-type TokenType string
-
-const (
-	ILLEGAL = "ILLEGAL"
-	EOF     = "EOF"
-	BUILTIN = "BUILTIN"
-
-	IDENT   = "IDENT"
-	INT     = "int"
-	FLOAT   = "float64"
-	STRING  = "str"
-	TRUE    = "true"
-	FALSE   = "false"
-	COMMENT = "COMMENT"
-
-	BEGIN = "BEGIN"
-	END   = "END"
-
-	ASSIGN     = "="
-	CMD_ASSIGN = "=cmd="
-	VAR_ASSIGN = "=var="
-	DEF_ASSIGN = "=def="
-	GVN_ASSIGN = "=gvn="
-	PVR_ASSIGN = "=pvr="
-	TYP_ASSIGN = "=typ="
-
-	COLON     = ":"
-	DOT       = "."
-	NEWLINE   = "\n"
-	SEMICOLON = ";"
-
-	AND = "and"
-	OR  = "or"
-	NOT = "not"
-
-	EQ     = "=="
-	NOT_EQ = "!="
-
-	DOUBLESLASH = "//"
-
-	DOTDOT = ".."
-
-	RIGHTARROW = "->"
-
-	NO_INDENT = "|||"
-
-	COMMA      = ","
-	WEAK_COMMA = ",,"
-
-	LPAREN = "("
-	RPAREN = ")"
-	LBRACE = "{"
-	RBRACE = "}"
-	LBRACK = "["
-	RBRACK = "]"
-
-	IMPORT  = "import"
-	VAR     = "var"
-	CMD     = "cmd"
-	DEF     = "def"
-	PRIVATE = "private"
-
-	RETURN = "return"
-	ELSE   = "else"
-	GIVEN  = "given"
-	EVAL   = "eval"
-	EXEC   = "exec"
-
-	MAGIC_COLON = "MAGIC_COLON"
-
-	GOLANG = "golang"
-)
+type Type string
 
 type Token struct {
-	Type    TokenType
+	Type    Type
 	Literal string
-	Line    int
-	ChStart int
-	ChEnd   int
-	Source  string
 }
 
-var keywords = map[string]TokenType{
-	"true":   TRUE,
-	"false":  FALSE,
-	"else":   ELSE,
-	"return": RETURN,
+const (
+	AND             = "&&"
+	ASSIGN          = "="
+	ASTERISK        = "*"
+	ASTERISK_EQUALS = "*="
+	BACKTICK        = "`"
+	BANG            = "!"
+	CASE            = "case"
+	COLON           = ":"
+	COMMA           = ","
+	CONST           = "FINAL"
+	CONTAINS        = "~="
+	DEFAULT         = "DEFAULT"
+	DEFINE_FUNCTION = "DEFINE_FUNCTION"
+	DOTDOT          = ".."
+	ELSE            = "ELSE"
+	EOF             = "EOF"
+	EQ              = "=="
+	FALSE           = "FALSE"
+	FLOAT           = "FLOAT"
+	FOR             = "FOR"
+	FOREACH         = "FOREACH"
+	FUNCTION        = "FUNCTION"
+	GT              = ">"
+	GTEQUALS        = ">="
+	IDENT           = "IDENT"
+	IF              = "IF"
+	ILLEGAL         = "ILLEGAL"
+	IN              = "IN"
+	INT             = "INT"
+	LBRACE          = "{"
+	LBRACKET        = "["
+	LET             = "LET"
+	LPAREN          = "("
+	LT              = "<"
+	LT_EQUALS       = "<="
+	MINUS           = "-"
+	MINUS_EQUALS    = "-="
+	MINUS_MINUS     = "--"
+	MOD             = "%"
+	NOT_CONTAINS    = "!~"
+	NOT_EQ          = "!="
+	NULL            = "null"
+	OR              = "||"
+	PERIOD          = "."
+	PLUS            = "+"
+	PLUS_EQUALS     = "+="
+	PLUS_PLUS       = "++"
+	POW             = "**"
+	QUESTION        = "?"
+	RBRACE          = "}"
+	RBRACKET        = "]"
+	REGEXP          = "REGEXP"
+	RETURN          = "RETURN"
+	RPAREN          = ")"
+	SEMICOLON       = ";"
+	SLASH           = "/"
+	SLASH_EQUALS    = "/="
+	STRING          = "STRING"
+	SWITCH          = "switch"
+	TRUE            = "TRUE"
+)
 
-	"eval":   EVAL,
-	"given":  GIVEN,
-	"exec":   EXEC,
-	"golang": GOLANG,
-
-	"import":  IMPORT,
-	"var":     VAR,
-	"cmd":     CMD,
-	"def":     DEF,
-	"private": PRIVATE,
-
-	"and": AND,
-	"or":  OR,
-	"not": NOT,
-
-	"=cmd=": CMD_ASSIGN,
+var keywords = map[string]Type{
+	"case":     CASE,
+	"final":    CONST,
+	"default":  DEFAULT,
+	"else":     ELSE,
+	"false":    FALSE,
+	"fun":      FUNCTION,
+	"for":      FOR,
+	"foreach":  FOREACH,
+	"function": DEFINE_FUNCTION,
+	"if":       IF,
+	"in":       IN,
+	"let":      LET,
+	"null":     NULL,
+	"return":   RETURN,
+	"switch":   SWITCH,
+	"true":     TRUE,
 }
 
-func LookupIdent(ident string) TokenType {
-	if tok, ok := keywords[ident]; ok {
+func LookupIdentifier(identifier string) Type {
+	if tok, ok := keywords[identifier]; ok {
 		return tok
 	}
 	return IDENT
-}
-
-func TokenTypeIsHeadword(t TokenType) bool {
-	return t == IMPORT || t == VAR || t == CMD || t == DEF
 }
